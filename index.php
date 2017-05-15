09acaa25575e329460b834cb9c841c23151960d6
Michael Harrison <mharrison13@cnm.edu>
2017-05-15 00:02:48 -0600

    bootstrap challenge

--- index.php
+++ index.php
@@ -48,13 +48,13 @@
 				</div>
 			</header>
 			<main>
-			<div class="row">
-				<div class="col-xs-6 col-md-3">
-					<a href="#" class="thumbnail">
-						<img src="..." alt="...">
-					</a>
+				<div class="row">
+					<div class="col-xs-6 col-md-3">
+						<a href="#" class="thumbnail">
+							<img src="..." alt="...">
+						</a>
+					</div>
+					...
 				</div>
-				...
-			</div>
 			</main>
 
